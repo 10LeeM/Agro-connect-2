35b6c7fe13eb28c7e6bc5d51961cdf98c3deb404
ron <Nelly Chelaa>
2020-12-20 17:42:32 +0300

    Agro-connect

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import HomePage from './Components/HomePage'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>Agro-Connect</p> 
+      </header>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
