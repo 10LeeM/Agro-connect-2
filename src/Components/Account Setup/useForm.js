2172ed23bf0274390e68305a59ebf1c25bdbd2fc
Nelly Chelaa <40514095+10LeeM@users.noreply.github.com>
2020-12-30 13:54:54 +0300

    Add files via upload

---
+++ src/Components/Account Setup/useForm.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const useForm = () => {
+    const [values, setValues] = useState ({
+        newBuyer: {
+            name:'',
+            email:'',
+            location:'',
+            operationScale: ''
+        }
+    });
+    
+
+    const [errors, setErrors] = useState ({});
+
+    const handleChange = e => {
+        const {name , value} = e.target
+        setValues ({
+            ...values,
+           [name] : value
+        })
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+    return (handleChange, values, handleSubmit);
+}
+
+export default useForm
