30b69f3744231e67b061569de22aed80c7dce8d6
Nelly Chelaa <40514095+10LeeM@users.noreply.github.com>
2021-01-28 22:17:54 +0300

    Add files via upload

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,24 +1,37 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import {NavLink, Link} from "react-router-dom";
+import '../Components/UpperComponents.css'
 
 function HeaderComponent() {
     return (
         //Agro-connect navigation links
-        <nav>
+        <div className="header-container">
+            <nav>
             <NavLink exact activeClassName="active" to="/">
                 Home
             </NavLink>
-            <NavLink exact activeClassName="active" to="services">
+            <NavLink exact activeClassName="active" to="">
                 Services
             </NavLink>
             <NavLink exact activeClassName="active" to="smartMarket">
                 Smart Market
             </NavLink>
             <NavLink exact activeClassName="active" to="contact">
+                FAQ
+            </NavLink>
+            <NavLink exact activeClassName="active" to="contact">
                 Contact US
             </NavLink>
-            
         </nav>
+        <div className="header-buttons">
+            <Link path="Account Setup/BuyersSignupForm">Sign in</Link>
+            <Link path="">Login</Link>
+            <Link path="">Cart</Link>
+        </div>
+        </div>
+        
+
+
     )
 }
 
